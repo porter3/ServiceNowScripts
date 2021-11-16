bef4faa812259ca35c836cb4ec847b6cd37a8b1d
Jake Porter <jacob.porter@cdw.com>
2021-11-16 17:38:41 -0500

    rename/repurpose file to fit story

--- Record Creation/create_incident_task.js
+++ Record Creation/create_incident_task.js
@@ -1,23 +1,22 @@
 // 11-16-2012
 
-// if a record is created on the weekend, create a new one and assign it to 'assigning_to'
+// if an incident is created on the weekend, create an incident task and assign it to 'assigning_to'
 
 (function executeRule(current, previous /*null when async*/) {
 
-	var table = 'incident';
 	var assigning_to = 'fred.luddy';
 
 	// getDayOfWeekLocalTime() starts at 1 for Monday and goes to 7 for Sunday
 	var day_of_week_num = new GlideDateTime().getDayOfWeekLocalTime();
 	if (day_of_week_num == 6 || day_of_week_num == 7) {
-		// create new incident
-		var gr = new GlideRecord(table);
+		var gr = new GlideRecord('incident_task');
 		gr.initialize();
-		gr.get(current.sys_id);
-		gr.number = new NumberManager(table).getNextObjNumberPadded();
-		gr.assignment_group = current.assignment_group;
+		gr.incident = current.sys_id;
+		gr.assignment_group = gs.getProperty('cdw.ag.hardware');
 		gr.assigned_to = assigning_to;
 		gr.insert();
+
+		gs.addInfoMessage("Incident task " + gr.number + " created");
 	}
 	
 })(current, previous);
